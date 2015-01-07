108bed32af8ecc8b855ac8bbe2f7ff53606383a9
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-01-07 15:24:38 -0500

    changed the way pipette resistance is measured

--- testExpts/measurePipetteResistance.m
+++ testExpts/measurePipetteResistance.m
@@ -3,7 +3,32 @@ function pipetteResistance = measurePipetteResistance
 
     data = acquireTrial;
     
-    voltDiff = max(data.voltage) - min(data.voltage);
-    currDiff = max(data.current) - min(data.current); 
+    highVoltageLog1 = data.voltage > mean(data.voltage);
+    
+    allPulseStarts = strfind(highVoltageLog1',[0 1]);
+    pulseStart = allPulseStarts(1) + 1;
+    allPulseEnds = strfind(highVoltageLog1',[1 0]);
+    pulseEnd = allPulseEnds(1); 
+    if pulseEnd < pulseStart
+        pulseEnd = allPulseEnds(2); 
+    end     
+    pulseEnd = pulseEnd - 3; 
+    pulseMid = round(pulseEnd - ((pulseEnd - pulseStart)/2));
+
+    troughStart = pulseEnd + 1; 
+    troughEnd = allPulseStarts(2) -3 ;
+    troughMid = round(troughEnd - ((troughEnd - troughStart)/2));
+    
+    peakCurrent = mean(data.current(pulseMid:pulseEnd));
+    peakVoltage = mean(data.voltage(pulseMid:pulseEnd));
+    baselineCurrent = mean(data.current(troughMid:troughEnd));
+    baselineVoltage = mean(data.voltage(troughMid:troughEnd));
+    
+    voltDiff = peakVoltage - baselineVoltage;
+    currDiff = peakCurrent - baselineCurrent; 
     
     pipetteResistance = ((voltDiff*1e-3)/(currDiff*1e-12))/1e6;  
+    
+
+
+
