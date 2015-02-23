3f8724617e3bfd0fec18034658065b148b48a075
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-02-22 19:51:55 -0500

    new stim sets

---
+++ stimSets/stimSet_007.m
@@ -0,0 +1,40 @@
+function stimSet_007(exptInfo,preExptData)
+
+% Produces the default pip train while switching between all three speakers
+
+%% Archive this code
+archiveExpCode(exptInfo)
+
+%% Set up and acquire with the stimulus set
+numberOfStimuli = 1;
+stimRan = randperm(numberOfStimuli);
+
+count = 1;
+FS = stoploop('Stop Experiment');
+while ~FS.Stop()
+    trialMeta.stimNum = stimRan(count);
+    fprintf(['\nStimNum = ',num2str(trialMeta.stimNum)])
+    stim = pickStimulus(trialMeta.stimNum);
+    switchSpeaker(stim.speaker);
+    acquireTrial('i',stim,exptInfo,preExptData,trialMeta);
+    if count == numberOfStimuli
+        count = 1;
+        stimRan = randperm(numberOfStimuli);
+    else
+        count = count+1;
+    end
+end
+
+FS.Clear() ;  % Clear up the box
+clear FS ;
+
+    function stim = pickStimulus(stimNum)
+        switch stimNum
+            case 1
+                stim = PipStimulus;
+                stim.speaker = 2;           
+        end
+    end
+
+
+end
