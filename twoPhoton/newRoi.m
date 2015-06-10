12c8a9321eee102d59f49e31225d455105558d86
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-06-10 11:01:55 -0400

    newRoi

--- twoPhoton/newRoi.m
+++ twoPhoton/newRoi.m
@@ -1,6 +1,4 @@
-function roiNum = getRoiNum
-
-newRoi = input('New ROI? ','s');
+function roiNum = newRoi
 
 roiNum = 1; 
 
@@ -14,8 +12,9 @@ if ~strcmp(newRoi,'y') && roiNum ~= 1
     roiNum = roiNum -1; 
 end 
 
-setpref('scimSavePrefs','roiNum',roiNum)
-fprintf(['ROI Number = ',num2str(roiNum),'\n'])
+setpref('scimSavePrefs','roiNum',roiNum);
+fprintf(['ROI Number = ',num2str(roiNum),'\n']);
 
 roiDescrip = input('Give ROI description: ');
 setpref('scimSavePrefs','roiDescrip',roiDescrip);
+
