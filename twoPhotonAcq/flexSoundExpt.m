23b9b4351d3755b3210b96268d66472296096d55
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-06-24 11:16:03 -0400

    better default stim

--- twoPhotonAcq/flexSoundExpt.m
+++ twoPhotonAcq/flexSoundExpt.m
@@ -2,13 +2,14 @@
 % AVB 2015/06/09
 
 %% Run at start of experiment
-runTwoPhotonExpt('test',1)
+runTwoPhotonExpt('12B10',1)
 
 %% AMTone
 stim = AmTone;
-stim.waveDur = 20;
-stim.startPadDur = 20;
-stim.endPadDur = 20;
+stim.waveDur = 10;
+stim.startPadDur = 5;
+stim.endPadDur = 5;
+stim.maxVoltage = 2; 
 plot(stim)
 
 %% Chirp
@@ -32,6 +33,9 @@ plot(stim)
 
 %% Pip
 stim = PipStimulus;
+stim.startPadDur = 5; 
+stim.endPadDur = 5; 
+stim.numPips = 30; 
 plot(stim)
 
 %% Pulse Song
@@ -47,7 +51,7 @@ metaFileName = acquireTwoPhotonTrial(stim);
 postMultTrialPlot(metaFileName)
 
 %% Run 5 trials
-for i = 1:2
+for i = 1:5
     if i == 1
         getRoiNum;
         getBlockNum;
