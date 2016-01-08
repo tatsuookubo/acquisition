eaf10adfeefecb9ba98caec779985d71f0ca2698
avbatchelor <alexbatchelor@hotmail.co.uk>
2016-01-08 09:53:24 -0500

    pvpp codee

---
+++ twoPhotonAcq/pvpp.m
@@ -0,0 +1,63 @@
+function pvpp
+
+%% Find out the folder
+folder = uigetdir;
+
+%% Make sure folder is empty
+cd(folder)
+movieFileNames = dir('*.tif');
+
+%% Load movie
+numMovies = length(movieFileNames);
+for i = 1:numMovies
+    % Get movie info
+    imageFileName = movieFileNames(i).name;
+    warning('off','MATLAB:imagesci:tiffmexutils:libtiffWarning')
+    imInfo = imfinfo(imageFileName);
+    powerLevels(i) = str2num(char(regexp(imInfo(1).ImageDescription,'(?<=state.init.eom.maxPower=).*(?=state.init.eom.usePowerArray)','match')));
+    frameRate = str2num(char(regexp(imInfo(1).ImageDescription,'(?<=state.acq.frameRate=).*(?=state.acq.zoom)','match')));
+    chans = regexp(imInfo(1).ImageDescription,'state.acq.acquiringChannel\d=1');
+    numChans = length(chans);
+    numFrames = round(length(imInfo)/numChans);
+    im1 = imread(imageFileName,'tiff','Index',1,'Info',imInfo);
+    numPx = size(im1);
+    
+    % Read in movie
+    mov = zeros([numPx(:); numChans; numFrames; numMovies]', 'double');  %preallocate 3-D array
+    try
+        for frame=1:numFrames
+            for chan = 1:numChans
+                [mov(:,:,chan,frame)] = imread(imageFileName,'tiff',...
+                    'Index',(2*(frame-1)+chan),'Info',imInfo);
+            end
+        end
+    catch
+        numFrames = numFrames -1;
+        for frame=1:numFrames
+            for chan = 1:numChans
+                [mov(:,:,chan,frame)] = imread(fullfile(pathName,imageFileName),'tiff',...
+                    'Index',(2*(frame-1)+chan),'Info',imInfo);
+            end
+        end
+    end
+
+    
+    %% Image processing
+    % Remove the last line where the image doesn't change
+    mov(64,:,:,:,:)=[];
+    
+    %% Separate channels
+    greenMov = squeeze(mov(:,:,2,:,:));
+    
+    movieMeans(i) = mean(greenMov(:)); 
+    movieVars(i) = var(greenMov(:));
+    
+end
+    
+figure
+plot(movieMeans,movieVars)
+xlabel('mean')
+ylabel('variance')
+a = powerLevels'; b = num2str(a); c = cellstr(b);
+dx = 0.01; dy = 0.1; % displacement so the text does not overlay the data points
+text(movieMeans+dx, movieVars+dy, c);
